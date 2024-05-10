c8adfe4e05ac7c74fc0eb4bb0610fad097ecd168
DiegoRomero07 <92567340+DiegoRomero07@users.noreply.github.com>
2024-05-10 15:09:39 -0500

    Agrego archivo Proyecto

---
+++ Proyecto/src/main/java/com/NowtifyLogist/Proyecto/NowtifyApplication.java
@@ -0,0 +1,13 @@
+package com.NowtifyLogist.Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowtifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowtifyApplication.class, args);
+	}
+
+}
